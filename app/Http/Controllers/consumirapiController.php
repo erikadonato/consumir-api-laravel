d2aefdce11730fce1bf369a5e6f9d9379560e3b0
Erika Donato <erikaadonato@gmail.com>
2022-01-03 21:43:13 -0300

    first commit

---
+++ app/Http/Controllers/consumirapiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class consumirapiController extends Controller
+{
+    //
+    public function index()
+    {
+        $response = Http::get('https://api.github.com/users/binascohub/repos');
+
+        dd($response->json());
+    }
+}
